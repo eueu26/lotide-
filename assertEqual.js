d9a95fb42da88bfca89168088663289fa1ddd002
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-02-08 12:59:38 -0500

    Implementing assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual !== expected) {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+      
+  }
+  
+};
+
+assertEqual("lighthouses Labs", "Bootcamp");
+assertEqual(1, 1);
