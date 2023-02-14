2149a8096f2ec4e439172a933bc274cc925dba79
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-02-13 20:32:36 -0500

    comparing two arrays and print strings

--- eqArrays.js
+++ eqArrays.js
@@ -21,11 +21,8 @@ const eqArrays = function(array1, array2) {
     if (array1[i] !== array2[i]) {
       return false;
     }
-    else {
-      return true;
-       }
       }
-    return;
+    return true;
     }
 };
 
