af6cce42cbec99772d27cce560787cea46b847f6
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-02-17 20:39:59 -0500

    create a version of maps for assertArraysEqaul

---
+++ map.js
@@ -0,0 +1,31 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("The arrays are the same");
+  } else {
+    console.log("The arrays are different");
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+assertArraysEqual(map(words, (item) => item.toLowerCase()), ['ground', 'control', 'to', 'major', 'tom']);
