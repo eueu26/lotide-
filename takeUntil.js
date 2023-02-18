9d97365da04d44d8ae1dcbc834060643c043b94e
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-02-17 21:17:50 -0500

    keep collecting items from a provided array until the callback provided returns a truthy value.

---
+++ takeUntil.js
@@ -0,0 +1,39 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("The arrays are the same");
+  } else {
+    console.log("The arrays are different");
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+
+const takeUntil = function(array, callback) {
+
+  const results = [];
+  for (let item of array) {
+    if (callback(item)) {
+      break;
+    } else {
+      results.push(item);
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(takeUntil(data1, (item) => item < 0), [1, 2, 4, 7, 2]);
+assertArraysEqual(takeUntil(data2, item => item === ","), ['I\'ve', 'been', 'to', 'Hollywood' ]);
