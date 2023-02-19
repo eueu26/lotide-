5cf320dea2d812b0adda42db588708c2dec35bd5
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-02-18 21:36:39 -0500

    Exported module

--- eqArrays.js
+++ eqArrays.js
@@ -1,30 +1,16 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = require("./assertEqual");
 
-  if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-      
-  }
-  
-};
 const eqArrays = function(array1, array2) {
-
   if (array1.length !== array2.length) {
     return false;
-  }
-  else {
-      
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
       }
-    return true;
     }
+    return true;
+  }
 };
 
-console.log(eqArrays([1,2,3], [1,2,3]));
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
