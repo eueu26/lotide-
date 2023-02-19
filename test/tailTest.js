fefaf03e882331c3002cbdb6076aee4d5e814af7
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-02-19 14:10:33 -0500

    Refactor to use Mocha & Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,19 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
 
-assertEqual(tail([5,6,7]).length, 2);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+// assertEqual(tail([5,6,7]).length, 2);
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+});
