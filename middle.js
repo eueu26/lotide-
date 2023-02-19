2d3a3363461563eb982f633038820f9989577a03
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-02-18 22:02:31 -0500

    Refactored middle

--- middle.js
+++ middle.js
@@ -1,33 +1,7 @@
-const eqArrays = function(array1, array2) {
 
-  if (array1.length !== array2.length) {
-    
-    return false;
-  }
-  else {
-      
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      } 
-    }
-    return true;
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
   
-const assertArraysEqual = function(array1, array2) {
-  
-  if (eqArrays(array1, array2)) {
-    console.log("The arrays are the same");
-  }
-  else {
-    console.log("The arrays are different");
-  }
-};
-  
-  //console.log(eqArrays([1,2,3], [1,2,3]));
-  //assertArraysEqual([1, 2, 3], [1, 2, 3]);
-
+ 
 const middle = function(array) {
   let length = array.length;
   let middle = Math.floor(length / 2);
@@ -47,8 +21,4 @@ const middle = function(array) {
   return middleNumbers;
 };
 
-assertArraysEqual(middle([1, 2, 3]), []);
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);
-assertArraysEqual(middle([1,2,3,4,5,]), [3]);
-assertArraysEqual(middle([1,2]), [1,2]);
-assertArraysEqual(middle([1]), []);
+module.exports = middle;
